eda390c03719ec77365b84cd489d7c75b99bfd43
Alexander Leonhardt <equinox.salexander@gmail.com>
2021-06-10 09:00:18 +0200

    Added an update which allows to limit the batch size to a specific value in order to estimate the ressource consumption independently of the text size. At the moment it is set to 16 which is a senisble default on my system, this may be changed later on to be loaded from the config

--- src/parse_service.py
+++ src/parse_service.py
@@ -50,6 +50,7 @@ for col in keep_columns:
     annotation_layers[col] = {"type": "TagSequence", "source_column": col}
 column_mapping = {annotation_id: annotation_layer["source_column"] for annotation_id, annotation_layer in annotation_layers.items()}
 
+maximum_batch_size = 16
 
 # API
 app = FastAPI()
@@ -68,11 +69,13 @@ def process(request: StepsParserRequest) -> StepsParserResponse:
     # parse
     dataset = CustomCoNLLDataset.from_corpus_file(conll_stream, annotation_layers)
     results = []
-    multi_parser = parser.parse_multi(dataset)
-    for (parsed_sentence,sentence) in zip(multi_parser,dataset):
-        for col in keep_columns or []:
-            parsed_sentence.annotation_data[col] = sentence[col]
-        results.append(parsed_sentence.to_conll(column_mapping))
+    for chunks in range(0,len(dataset),maximum_batch_size):
+        partitioned_dataset = dataset[chunks:(chunks+64)]
+        multi_parser = parser.parse_multi(partitioned_dataset)
+        for (parsed_sentence,sentence) in zip(multi_parser,partitioned_dataset):
+            for col in keep_columns or []:
+                parsed_sentence.annotation_data[col] = sentence[col]
+            results.append(parsed_sentence.to_conll(column_mapping))
 
     conll_stream.close()
 
